32ea797fa42af5708c295fb03520af4fb4210b1a
tscalfaro <tscalfaro7@gmail.com>
2021-12-15 22:50:32 -0500

    Contact page coming along, icons in.

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -5,13 +5,24 @@ export const Contact = () => {
   return (
     <div className="contact-div">
       <div className="contact-left-side">
-      <h1>Reach out, get in touch</h1>
+      <h2 id="reach-out">Reach out</h2>
       </div>
 
       <div className="contact-right-side">
-        <span ><h3 id="email-header">Email:</h3></span> <span><h3 id="mobile-header">Mobile:</h3></span>
+        <span ><h3 id="email-header">Email</h3></span>
         <p id="email">tscalfaro7@gmail.com</p>
+        <span><h3 id="mobile-header">Mobile</h3></span>
+
         <p id="mobile">215 - 460 - 4680</p>
+
+      </div>
+      <div className="contact-icons">
+        <div>
+        <a href="https://github.com/tscalfaro/website"><i className="fa fa-github contact-icons fa-2x"></i></a>
+        </div>
+        <div>
+        <a href="https://www.linkedin.com/in/antonioscalfaro/"><i className="fa fa-linkedin contact-icons fa-2x"></i></a>
+        </div>
       </div>
     </div>
   )
