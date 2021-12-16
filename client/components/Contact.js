5935dd8d5390bc1064b9c6d11b5411a58630d7f9
tscalfaro <tscalfaro7@gmail.com>
2021-12-15 21:19:43 -0500

    contact page hover

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -4,12 +4,14 @@ import { connect } from "react-redux";
 export const Contact = () => {
   return (
     <div className="contact-div">
-      <div>
-      <h3>Reach out, get in touch</h3>
+      <div className="contact-left-side">
+      <h1>Reach out, get in touch</h1>
       </div>
 
-      <div>
-
+      <div className="contact-right-side">
+        <span ><h3 id="email-header">Email:</h3></span> <span><h3 id="mobile-header">Mobile:</h3></span>
+        <p id="email">tscalfaro7@gmail.com</p>
+        <p id="mobile">215 - 460 - 4680</p>
       </div>
     </div>
   )
