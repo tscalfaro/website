7b1de47492915f5376ca7c99993d991d4aea9112
Tony Scalfaro <tscalfaro7@gmail.com>
2021-12-22 13:39:37 -0500

    add minify to image webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const path = require("path")
 module.exports = {
   entry: ["./client/index.js"],
   output: {
-    path: path.resolve(__dirname, "public"),
     filename: "./public/bundle.js",
+    path: path.resolve(__dirname, "public"),
   },
   devtool: "source-map",
   module: {
@@ -20,13 +20,49 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ["file-loader"],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [ImageMinimizerPlugin,"file-loader"],
       },
     ],
   },
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                "svgo",
+                {
+                  plugins: extendDefaultPlugins([
+                    {
+                      name: "removeViewBox",
+                      active: false,
+                    },
+                    {
+                      name: "addAttributesToSVGElement",
+                      params: {
+                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                      },
+                    },
+                  ]),
+                },
+              ],
+            ],
+          },
+        },
+      })
+    ]}
 };
