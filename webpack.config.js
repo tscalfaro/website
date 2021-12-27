0f6c3543e22d3805a34ab3d653d56db33e52ca85
Tony Scalfaro <tscalfaro7@gmail.com>
2021-12-27 14:21:52 -0500

    images seem to be working normally nw

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: ["./client/index.js"],
   output: {
     filename: "./public/bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: __dirname,
   },
   devtool: "source-map",
   module: {
@@ -25,44 +25,9 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: [ImageMinimizerPlugin,"file-loader"],
+        use: ["file-loader"],
       },
     ],
   },
-  optimization: {
-    minimizer: [
-      "...",
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                "svgo",
-                {
-                  plugins: extendDefaultPlugins([
-                    {
-                      name: "removeViewBox",
-                      active: false,
-                    },
-                    {
-                      name: "addAttributesToSVGElement",
-                      params: {
-                        attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-                      },
-                    },
-                  ]),
-                },
-              ],
-            ],
-          },
-        },
-      })
-    ]}
+  
 };
